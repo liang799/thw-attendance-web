5e51abd9a67835d663dfe5d6f9f162aa8fb9f2d1
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-01 22:04:08 +0800

    Added validation to form

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -13,14 +13,21 @@ import {
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { ApiClient } from "@/utils/axios";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  rank: yup.string().uppercase().required(),
+  name: yup.string().lowercase().required()
+});
 
 export default function RegisterPage() {
   const toast = useToast();
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({ resolver: yupResolver(schema) });
   const onSubmit = async (data) => {
     try {
       const response = await ApiClient.post("/users", data);
-    } catch (error) {
+    } catch (error: Error) {
       toast({
         title: error.name,
         description: error.message,
@@ -37,15 +44,15 @@ export default function RegisterPage() {
         <Heading p={5}>Registration</Heading>
         <Stack bg={useColorModeValue("white", "gray.700")} p={5}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl>
-              <FormLabel>Rank</FormLabel>
-              <Input {...register("rank", { required: true })} />
-              <FormErrorMessage>{errors.rank && errors.rank.message}</FormErrorMessage>
+            <FormControl isInvalid={errors.rank}>
+              <FormLabel htmlFor="rank">Rank</FormLabel>
+              <Input {...register("rank")} />
+              <FormErrorMessage>{errors.rank?.message}</FormErrorMessage>
             </FormControl>
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <Input {...register("name", { required: true })} />
-              <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+            <FormControl isInvalid={errors.name}>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <Input {...register("name")} />
+              <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
             </FormControl>
             <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
               Submit
