42d8016b6e2948a1760a199e6ef3bf8d30d155d1
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-11 21:30:28 +0800

    UI Changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,72 @@
-import { Inter } from "next/font/google";
-import { useQuery } from "react-query";
-import { ReactQueryKey } from "@/utils/react-query-keys";
+import {
+  Text,
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Link,
+  useColorModeValue,
+  useToast
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 import { ApiClient } from "@/utils/axios";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import PasswordInput from "@/components/PasswordInput";
+import { useRouter } from "next/navigation";
 
-const inter = Inter({ subsets: ['latin'] })
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required()
+});
 
-export default function Home() {
-  const {data} = useQuery(ReactQueryKey.TEST, () => ApiClient.get("/"))
+export default function RegisterPage() {
+  const toast = useToast();
+  const router = useRouter();
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({ resolver: yupResolver(schema) });
+  const onSubmit = async (data) => {
+    try {
+      const response = await ApiClient.post("/users", data);
+      router.push(`/onboard-user/${response.data.id}`)
+    } catch (error: Error) {
+      toast({
+        title: error.name,
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true
+      });
+    }
+  };
+
   return (
-    <div>{JSON.stringify(data)}</div>
+    <Container maxW="container.xl" minH="100vh" bg={useColorModeValue("gray.50", "gray.800")}>
+      <Flex p={5} align="center" justify="center" flexDirection="column">
+        <Heading p={5}>Registration</Heading>
+        <Stack bg={useColorModeValue("white", "gray.700")} p={5}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.email}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input {...register("email")} />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.password}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <PasswordInput {...register("password")} />
+              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+            </FormControl>
+            <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+              Submit
+            </Button>
+          </form>
+          <Text align="center">Already a user? <Link href="/auth/login">Login</Link></Text>
+        </Stack>
+      </Flex>
+    </Container>
   );
 }
