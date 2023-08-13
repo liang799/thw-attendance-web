59976ed7e3cab3a54fff440152338ee15f8f5a15
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-13 16:58:06 +0800

    Made /ongoing-parade api private again

--- backend/src/api/ongoing-parade/ongoing-parade.controller.ts
+++ backend/src/api/ongoing-parade/ongoing-parade.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Get } from "@nestjs/common";
 import { OngoingParadeService } from './ongoing-parade.service';
-import { Public } from "../../constants";
 
 @Controller('ongoing-parade')
 export class OngoingParadeController {
   constructor(private readonly ongoingParadeService: OngoingParadeService) {}
-  @Public()
   @Get()
   getLatestOngoingParade() {
     return this.ongoingParadeService.getLatestOngoingParade();
