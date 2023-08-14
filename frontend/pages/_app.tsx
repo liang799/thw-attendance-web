477551dde3739b78c295b778806363815607e7ef
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-14 11:10:57 +0800

    Deleted react buddy usage

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AppProps } from "next/app";
-import { DevSupport } from "@react-buddy/ide-toolbox-next";
-import { ComponentPreviews, useInitial } from "@/components/dev";
 
 const queryClient = new QueryClient();
 
@@ -10,11 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-          <Component {...pageProps} />
-        </DevSupport>
+        <Component {...pageProps} />
       </ChakraProvider>
     </QueryClientProvider>
   );
