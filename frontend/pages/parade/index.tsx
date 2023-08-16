30785032154106fe25fccc32208bb38e5ba9bd75
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-16 20:25:35 +0800

    Performance improvements by using NextLink in /parade UI

--- frontend/pages/parade/index.tsx
+++ frontend/pages/parade/index.tsx
@@ -1,26 +1,27 @@
-import HorizontalCard from "@/components/HorizontalCard";
-import { CardBody, Container, Heading, Link, Skeleton, Stack, useColorModeValue } from "@chakra-ui/react";
-import { useQuery } from "react-query";
-import { ReactQueryKey } from "@/utils/react-query-keys";
-import { ApiClient } from "@/utils/axios";
-import Navbar from "@/components/Navbar";
+import HorizontalCard from '@/components/HorizontalCard';
+import { CardBody, Container, Heading, Link, Skeleton, Stack, useColorModeValue } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+import { ReactQueryKey } from '@/utils/react-query-keys';
+import { ApiClient } from '@/utils/axios';
+import Navbar from '@/components/Navbar';
+import NextLink from 'next/link';
 
 export default function ParadeIndexPage() {
-  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const bgColor = useColorModeValue('gray.50', 'gray.800');
   const { data, isLoading, isError } = useQuery(ReactQueryKey.LATEST_PARADE,
     () => {
-      return ApiClient.get("/ongoing-parade")
+      return ApiClient.get('/ongoing-parade')
         .then(res => res.data);
-    }, { retry: false }
+    }, { retry: false },
   );
 
   if (isLoading) {
     return (
-      <Container p={4} maxW="container.xl" minH="100vh" bg={bgColor}>
+      <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
         <Navbar />
-        <Stack p={4} spacing="12px">
-          <Skeleton height="20px" />
-          <Skeleton height="20px" />
+        <Stack p={4} spacing='12px'>
+          <Skeleton height='20px' />
+          <Skeleton height='20px' />
         </Stack>
       </Container>
     );
@@ -28,14 +29,14 @@ export default function ParadeIndexPage() {
 
   if (isError) {
     return (
-      <Container p={4} maxW="container.xl" minH="100vh" bg={bgColor}>
+      <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
         <Navbar />
-        <Stack p={4} spacing="12px">
+        <Stack p={4} spacing='12px'>
           <Heading pb={4}>Parade State Tracker</Heading>
-          <Link href="/parade/create">
+          <Link href='/parade/create' as={NextLink}>
             <HorizontalCard>
               <CardBody>
-                <Heading size="md">Create Parade</Heading>
+                <Heading size='md'>Create Parade</Heading>
               </CardBody>
             </HorizontalCard>
           </Link>
@@ -45,24 +46,24 @@ export default function ParadeIndexPage() {
   }
 
   return (
-    <Container p={4} maxW="container.xl" minH="100vh" bg={bgColor}>
+    <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
       <Navbar />
 
-      <Stack p={4} spacing="12px">
+      <Stack p={4} spacing='12px'>
         <Heading pb={4}>Parade State Tracker</Heading>
 
-        <Link href="/submit-attendance">
+        <Link href='/submit-attendance' as={NextLink}>
           <HorizontalCard>
             <CardBody>
-              <Heading size="md">Submit Attendance</Heading>
+              <Heading size='md'>Submit Attendance</Heading>
             </CardBody>
           </HorizontalCard>
         </Link>
 
-        <Link href={`/parade/${data.id}`}>
+        <Link href={`/parade/${data.id}`} as={NextLink}>
           <HorizontalCard>
             <CardBody>
-              <Heading size="md">Overview</Heading>
+              <Heading size='md'>Overview</Heading>
             </CardBody>
           </HorizontalCard>
         </Link>
