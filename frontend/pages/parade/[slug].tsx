47e14bddfcbaec41500ca36b9c2e9717bd94ea74
liang799 <71926348+liang799@users.noreply.github.com>
2023-08-16 21:27:16 +0800

    Fixed clipboard issue and undefined api response data issue

--- frontend/pages/parade/[slug].tsx
+++ frontend/pages/parade/[slug].tsx
@@ -1,31 +1,31 @@
 import {
   CardBody,
   Container, Heading, Button, Link, Skeleton, Stack, useToast,
-  Text, useColorModeValue, useClipboard, IconButton
-} from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useQuery } from "react-query";
-import { ReactQueryKey } from "@/utils/react-query-keys";
-import { ApiClient } from "@/utils/axios";
-import GenericErrorDisplay from "@/components/GenericErrorDisplay";
-import { Attendance, GetAttendanceData } from "@/utils/types/AttendanceData";
-import HorizontalCard from "@/components/HorizontalCard";
-import { useState } from "react";
-import { DateTime } from "luxon";
-import AttendanceModal from "@/components/AttendanceModal";
-import Navbar from "@/components/Navbar";
+  Text, useColorModeValue, useClipboard,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { ApiClient } from '@/utils/axios';
+import GenericErrorDisplay from '@/components/GenericErrorDisplay';
+import { Attendance, GetAttendanceData } from '@/utils/types/AttendanceData';
+import HorizontalCard from '@/components/HorizontalCard';
+import { useState } from 'react';
+import { DateTime } from 'luxon';
+import AttendanceModal from '@/components/AttendanceModal';
+import Navbar from '@/components/Navbar';
 import { CopyIcon } from '@chakra-ui/icons';
+import generateParadeText from '@/utils/generateParadeText';
 
 function generateAttendanceStatus(data: GetAttendanceData) {
   const availability = data.status;
   switch (availability) {
-    case "Dispatch":
+    case 'Dispatch':
       return <Text>{`${data.status} - ${data.dispatchLocation}`}</Text>;
-    case "Present":
+    case 'Present':
       return <Text>{`${data.status}`}</Text>;
-    case "MC":
+    case 'MC':
       if (!data.mcEndDate) return <Text>{`${data.status} - No End date`}</Text>;
-      const date = DateTime.fromISO(data.mcEndDate).toFormat("ddLLyy");
+      const date = DateTime.fromISO(data.mcEndDate).toFormat('ddLLyy');
       return <Text>{`${data.status} - ${date}`}</Text>;
     default:
       return <Text>{data.status}</Text>;
@@ -33,98 +33,104 @@ function generateAttendanceStatus(data: GetAttendanceData) {
 }
 
 export default function ParadeIdPage() {
-  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const bgColor = useColorModeValue('gray.50', 'gray.800');
   const [attendance, setAttendance] = useState<Attendance | null>(null);
   const [showModal, setShowModal] = useState<boolean>(false);
   const router = useRouter();
   const { slug } = router.query;
-  const { onCopy, value, hasCopied, setValue } = useClipboard("");
+  const { onCopy, value, hasCopied, setValue } = useClipboard('');
   const toast = useToast();
 
-  const { data, isError } = useQuery(ReactQueryKey.LATEST_PARADE,
+  const { data, isError, isLoading } = useQuery(`Get Parade ${slug}`,
     () => {
-      if (!slug) return;
       return ApiClient.get(`/parades/${slug}`)
         .then(res => res.data);
-    }, { enabled: !!slug }
+    }, {
+      onSuccess: data => {
+        setValue(generateParadeText(data));
+      },
+    },
   );
 
   if (isError) {
     return (
       <>
         <Navbar />
-        <GenericErrorDisplay title="Error">Something went wrong</GenericErrorDisplay>
+        <GenericErrorDisplay title='Error'>Something went wrong</GenericErrorDisplay>
       </>
     );
   }
 
-  if (data) {
-    const handleClick = (attendance: Attendance) => {
-      setAttendance(attendance);
-      setShowModal(true);
-    };
+  const handleClick = (attendance: Attendance) => {
+    setAttendance(attendance);
+    setShowModal(true);
+  };
 
-    const generateCopiedContent = () => {
-      const copiedText: string = [
-        `Parade State Summary`,
-        `Node: THWHQ`,
-        `Start Time: ${DateTime.fromISO(data.startDate).toLocaleString(DateTime.DATETIME_FULL)}`,
-        `End Time: ${DateTime.fromISO(data.endDate).toLocaleString(DateTime.DATETIME_FULL)}`,
-        ...data.attendances.map((attendance: Attendance) => {
-          console.log(attendance?.id)
-          return `${attendance.user.rank} ${attendance.user.name} - ${attendance.availability.status}`;
-        }),
-      ].join('\n');
-      setValue(copiedText);
-      console.log(value);
-      onCopy();
+  const copyToClipboard = () => {
+    if (!value) {
       toast({
-        title: 'Copied!',
-        description: `You have copied Parade State ${data.id} to clipboard`,
-        status: 'success',
+        title: 'Error',
+        description: 'Unable to copy parade state. Please try again',
+        status: 'error',
         duration: 9000,
         isClosable: true,
-      })
-    };
+      });
+      return;
+    }
+    onCopy();
+    toast({
+      title: 'Copied!',
+      description: `You have copied Parade State ${data.id} to clipboard`,
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
 
+  if (isLoading || !data || !slug) {
     return (
-      <Container maxW="container.xl" minH="100vh" bg={bgColor}>
+      <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
         <Navbar />
-
-        <AttendanceModal attendance={attendance} showModal={showModal} setShowModal={setShowModal} />
-
-        <Stack spacing={4}>
-          <Heading>Parade State Summary</Heading>
-          <Button colorScheme='teal' leftIcon={<CopyIcon />} onClick={generateCopiedContent} width="200px">
-            {hasCopied ? "Copied!" : "Copy"}
-          </Button>
-          <Text>Node: THWHQ</Text>
-          <Text>Start Time: {DateTime.fromISO(data.startDate).toLocaleString(DateTime.DATETIME_FULL)}</Text>
-          <Text>End Time: {DateTime.fromISO(data.endDate).toLocaleString(DateTime.DATETIME_FULL)}</Text>
-          {data.attendances.map((attendance: Attendance) => {
-            return (
-              <Link key={attendance.id} onClick={() => handleClick(attendance)}>
-                <HorizontalCard key={attendance.id}>
-                  <CardBody>
-                    <Text>{`${attendance.user.rank} ${attendance.user.name}`}</Text>
-                    {generateAttendanceStatus(attendance.availability)}
-                  </CardBody>
-                </HorizontalCard>
-              </Link>
-            );
-          })
-          }
+        <Stack p={4} spacing='12px'>
+          <Skeleton height='20px' />
+          <Skeleton height='20px' />
         </Stack>
       </Container>
     );
   }
 
   return (
-    <Container p={4} maxW="container.xl" minH="100vh" bg={bgColor}>
+    <Container maxW='container.xl' minH='100vh' bg={bgColor}>
       <Navbar />
-      <Stack p={4} spacing="12px">
-        <Skeleton height="20px" />
-        <Skeleton height="20px" />
+
+      <AttendanceModal attendance={attendance} showModal={showModal} setShowModal={setShowModal} />
+
+      <Stack spacing={4}>
+        <Heading>Parade State Summary</Heading>
+        <Button
+          colorScheme='teal'
+          leftIcon={<CopyIcon />}
+          onClick={copyToClipboard}
+          width='200px'
+        >
+          {hasCopied ? 'Copied!' : 'Copy'}
+        </Button>
+        <Text>Node: THWHQ</Text>
+        <Text>Start Time: {DateTime.fromISO(data.startDate).toLocaleString(DateTime.DATETIME_FULL)}</Text>
+        <Text>End Time: {DateTime.fromISO(data.endDate).toLocaleString(DateTime.DATETIME_FULL)}</Text>
+        {data.attendances.map((attendance: Attendance) => {
+          return (
+            <Link key={attendance.id} onClick={() => handleClick(attendance)}>
+              <HorizontalCard key={attendance.id}>
+                <CardBody>
+                  <Text>{`${attendance.user.rank} ${attendance.user.name}`}</Text>
+                  {generateAttendanceStatus(attendance.availability)}
+                </CardBody>
+              </HorizontalCard>
+            </Link>
+          );
+        })
+        }
       </Stack>
     </Container>
   );
