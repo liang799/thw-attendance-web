549507e830de399429803c7bb58272975c8f26e1
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-04 13:09:14 +0800

    Added feature to sort people according to branch

--- backend/src/api/parades/dto/find-one-parade.dto.ts
+++ backend/src/api/parades/dto/find-one-parade.dto.ts
@@ -1,6 +1,7 @@
 import { ParadeType } from '../type/ParadeType';
 import { Attendance } from '../../attendances/entities/attendance.entity';
 import { Parade } from '../entities/parade.entity';
+import { BranchType } from '../../users/types/BranchType';
 
 export class FindOneParadeDto {
   id: number;
@@ -20,7 +21,8 @@ export class FindOneParadeDto {
     this.type = parade.type;
     this.startDate = parade.startDate;
     this.endDate = parade.endDate;
-    this.attendances = parade.attendances.getItems();
+    const rawAttendances = parade.attendances.getItems();
+    this.attendances = this.sortPeople(rawAttendances);
     this.summary = this.tallyAttendance(this.attendances)
   }
 
@@ -33,5 +35,19 @@ export class FindOneParadeDto {
     }
 
     return summary;
+  }
+
+  private sortPeople(attendances: Attendance[]): Attendance[] {
+    const branchesPriority: Record<string, number> = {};
+
+    branchesPriority[BranchType.COMMANDER] = 0;
+    branchesPriority[BranchType.S1] = 1;
+    branchesPriority[BranchType.S3] = 2;
+    branchesPriority[BranchType.S4] = 4;
+    branchesPriority[BranchType.TRANSITION] = 5;
+
+    return attendances.sort((a, b) => {
+      return branchesPriority[a.user.type] - branchesPriority[b.user.type]
+    })
   }
 }
