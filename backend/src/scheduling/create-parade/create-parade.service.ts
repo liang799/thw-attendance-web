f8c767bd473d523ab5a4636a97deea41ca11344b
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-04 13:48:02 +0800

    Added timezone to cron job

--- backend/src/scheduling/create-parade/create-parade.service.ts
+++ backend/src/scheduling/create-parade/create-parade.service.ts
@@ -8,10 +8,13 @@ import { UpdateParadeDto } from '../../api/parades/dto/update-parade.dto';
 export class CreateParadeService {
   constructor(
     private readonly paradeService: ParadesService,
-    ) {
+  ) {
   }
 
-  @Cron('0 0 8 * * 1-5') // Cron job For 8:00 AM on Monday to Friday
+  @Cron('0 0 8 * * 1-5', {
+    name: 'First Parade',
+    timeZone: 'Asia/Singapore',
+  }) // Cron job For 8:00 AM on Monday to Friday
   async createFirstParade() {
     const currentParade = await this.paradeService.getLatestOngoingParade();
     if (!currentParade) {
@@ -21,7 +24,10 @@ export class CreateParadeService {
     await this.paradeService.update(currentParade.id, UpdateParadeDto.firstParade());
   }
 
-  @Cron('30 13 * * 1-5') // Cron job For 1:30 PM on Monday to Friday
+  @Cron('30 13 * * 1-5', {
+    name: 'Mid Parade',
+    timeZone: 'Asia/Singapore',
+  }) // Cron job For 1:30 PM on Monday to Friday
   async createMidParade() {
     const currentParade = await this.paradeService.getLatestOngoingParade();
     if (!currentParade) {
