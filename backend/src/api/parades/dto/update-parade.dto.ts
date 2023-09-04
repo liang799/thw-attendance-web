d90c138299d3a313bf331cb60b42eaf4cb66ca52
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-04 13:17:59 +0800

    Fixed typescript error about dto

--- backend/src/api/parades/dto/update-parade.dto.ts
+++ backend/src/api/parades/dto/update-parade.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateParadeDto } from './create-parade.dto';
+import { ParadeType } from '../type/ParadeType';
 
-export class UpdateParadeDto extends PartialType(CreateParadeDto) {}
+export class UpdateParadeDto extends PartialType(CreateParadeDto) {
+  static firstParade() {
+    const dto = new UpdateParadeDto();
+    dto.startDate = new Date().toString();
+    dto.type = ParadeType.FIRST;
+    return dto;
+  }
+
+  static midParade() {
+    const dto = new UpdateParadeDto();
+    dto.startDate = new Date().toString();
+    dto.type = ParadeType.MID;
+    return dto;
+  }
+}
