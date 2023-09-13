72cdf3225c05a9f980ef6e77a3fc14c8e9e53709
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-13 10:28:39 +0800

    Automatically submit attendnance for absent

--- backend/src/api/parades/parades.service.ts
+++ backend/src/api/parades/parades.service.ts
@@ -7,6 +7,7 @@ import { Parade } from './entities/parade.entity';
 import { FindOneParadeDto } from './dto/find-one-parade.dto';
 import { User } from '../users/entities/user.entity';
 import { EntityManager } from '@mikro-orm/mysql';
+import { Attendance } from '../attendances/entities/attendance.entity';
 
 @Injectable()
 export class ParadesService {
@@ -14,12 +15,39 @@ export class ParadesService {
     private readonly repository: ParadeRepository,
     private readonly orm: MikroORM,
     private readonly em: EntityManager,
-  ) {
-  }
+  ) { }
 
   @UseRequestContext()
   async create(dto: CreateParadeDto) {
     const parade = new Parade(new Date(dto.startDate));
+    await this.em.persist(parade);
+
+    const prevParade = await this.repository.findOne(
+      { endDate: { $ne: null } },
+      {
+        orderBy: { id: QueryOrder.DESC },
+        populate: ['attendances', 'attendances.user'],
+      },
+    );
+
+    if (prevParade) {
+      console.log(prevParade);
+      const attendances = prevParade.attendances.getItems();
+      attendances.forEach(async (prevAttendance) => {
+        if (prevAttendance.availability.absentEndDate == null) {
+          const user = prevAttendance.user;
+          const attendance = user.createBlankTemplateAttendance(parade);
+          await this.em.persist(attendance);
+          return;
+        }
+        const attendance = new Attendance();
+        attendance.availability = prevAttendance.availability;
+        attendance.parade = parade;
+        attendance.user = prevAttendance.user;
+        await this.em.persist(attendance);
+      });
+    }
+
     const users = await this.em.find(User, {});
     if (!users) return this.em.persistAndFlush(parade);
     for (const user of users) {
@@ -37,7 +65,7 @@ export class ParadesService {
   @UseRequestContext()
   async findOne(id: number) {
     const parade = await this.repository.findOne(id, {
-      populate: ['attendances', 'attendances.user']
+      populate: ['attendances', 'attendances.user'],
     });
     const connection = this.em.getConnection();
     const knex = connection.getKnex();
@@ -46,7 +74,7 @@ export class ParadesService {
       .count('* as count')
       .from('attendance')
       .where('parade_id', parade.id)
-      .groupBy('availability_status')
+      .groupBy('availability_status');
     return new FindOneParadeDto(parade, availabilitySummary);
   }
 
