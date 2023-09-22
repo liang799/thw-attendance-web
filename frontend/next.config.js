edbbd6897bf46fbc057519529304967f43c91e76
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-22 19:06:05 +0800

    Disabled logging only in production

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    removeConsole: true,
+    removeConsole:
+      process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
   },
 }
 
