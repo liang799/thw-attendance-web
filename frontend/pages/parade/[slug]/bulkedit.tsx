77343cfff73a9ed9ddfc9f3e63eda286087bf535
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-25 15:14:29 +0800

    Added checkbox to bulk edit table

--- frontend/pages/parade/[slug]/bulkedit.tsx
+++ frontend/pages/parade/[slug]/bulkedit.tsx
@@ -1,8 +1,57 @@
+import { Checkbox, Heading } from '@chakra-ui/react';
+import { ParadeData } from '@/utils/types/ParadeData';
+import { ApiClient } from '@/utils/axios';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { Attendance } from '@/utils/types/AttendanceData';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
 import { DataTable } from '@/components/DataTable';
-import { Heading } from '@chakra-ui/react';
 
 export default function BulkEdit() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const { data: paradeData, isError, isLoading } = useQuery<ParadeData>(`Get Parade ${slug}`,
+    () => {
+      return ApiClient.get(`/parades/${slug}`)
+        .then(res => res.data);
+    }, {
+      enabled: !!slug,
+    },
+  );
+
+  if (isError) return <Heading>Error</Heading>;
+  if (isLoading || !slug || !paradeData) return <Heading>Loading</Heading>;
+
+  const columnHelper = createColumnHelper<Attendance>();
+  const columns: ColumnDef<Attendance>[] = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          {...{
+            isChecked: table.getIsAllRowsSelected(),
+            isIndeterminate: table.getIsSomeRowsSelected(),
+            onChange: table.getToggleAllRowsSelectedHandler(),
+          }}
+        />
+      ),
+      cell: ({ row, getValue }) => (
+        <Checkbox
+          {...{
+            isChecked: row.getIsSelected(),
+            isIndeterminate: row.getIsSomeSelected(),
+            onChange: row.getToggleSelectedHandler(),
+          }}
+        />
+      ),
+      footer: props => props.column.id,
+    },
+    columnHelper.accessor(row => `${row.user.rank} ${row.user.name}`, {
+      header: 'User',
+    }),
+  ];
   return (
-    <Heading>Todo</Heading>
+    <DataTable data={paradeData.attendances} columns={columns} />
   );
 }
