402a688db036dd477fcd2a7db67a92c331c72201
liang799 <71926348+liang799@users.noreply.github.com>
2023-09-26 09:26:57 +0800

    Made bulk edit ui prettier

--- frontend/pages/parade/[slug]/bulkedit.tsx
+++ frontend/pages/parade/[slug]/bulkedit.tsx
@@ -1,4 +1,17 @@
-import { Checkbox, Heading } from '@chakra-ui/react';
+import {
+  Button,
+  Checkbox,
+  Container,
+  Editable,
+  Heading,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { ParadeData } from '@/utils/types/ParadeData';
 import { ApiClient } from '@/utils/axios';
 import { useRouter } from 'next/router';
@@ -6,10 +19,13 @@ import { useQuery } from 'react-query';
 import { Attendance } from '@/utils/types/AttendanceData';
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
 import { DataTable } from '@/components/DataTable';
+import { ArrowForwardIcon, CheckCircleIcon, ChevronDownIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import Navbar from '@/components/Navbar';
 
 export default function BulkEdit() {
   const router = useRouter();
   const { slug } = router.query;
+  const bgColor = useColorModeValue('gray.50', 'gray.800');
 
   const { data: paradeData, isError, isLoading } = useQuery<ParadeData>(`Get Parade ${slug}`,
     () => {
@@ -49,9 +65,37 @@ export default function BulkEdit() {
     },
     columnHelper.accessor(row => `${row.user.rank} ${row.user.name}`, {
       header: 'User',
+      id: 'user',
+    }),
+    columnHelper.accessor(row => `${row.availability.status}`, {
+      cell: props => (
+        <Editable>{`${props}`}</Editable>
+      ),
+      id: 'test',
+      header: 'jaklds',
     }),
   ];
   return (
-    <DataTable data={paradeData.attendances} columns={columns} />
+    <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
+      <Navbar />
+      <Stack>
+        <Heading pt={4} px={4}>Bulk Edit</Heading>
+        <HStack px={4} pb={2}>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              With Selected
+            </MenuButton>
+            <MenuList>
+              <MenuItem icon={<CheckCircleIcon />}>Mark Present</MenuItem>
+              <MenuItem icon={<EditIcon />}>Set as...</MenuItem>
+              <MenuItem icon={<ArrowForwardIcon />}>Move to branch...</MenuItem>
+              <MenuItem icon={<DeleteIcon />}>Delete</MenuItem>
+            </MenuList>
+          </Menu>
+          <Button colorScheme='green'>Save</Button>
+        </HStack>
+        <DataTable data={paradeData.attendances} columns={columns} />
+      </Stack>
+    </Container>
   );
 }
