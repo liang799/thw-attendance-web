917e01a6986da85952f278fa8292160e3d099011
liang799 <71926348+liang799@users.noreply.github.com>
2024-01-15 14:07:20 +0800

    Improved user edit experience

--- frontend/pages/user/[slug].tsx
+++ frontend/pages/user/[slug].tsx
@@ -1,6 +1,9 @@
 import {
   Button,
   Container,
+  Editable,
+  EditableInput,
+  EditablePreview,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -31,11 +34,15 @@ import * as yup from 'yup';
 const schema = yup.object({
   userType: yup.string().required(),
   hasLeftNode: yup.boolean().required(),
+  rank: yup.string().required(),
+  name: yup.string().required(),
 });
 
 export type FormData = {
   userType: string,
   hasLeftNode: boolean,
+  rank: string,
+  name: string,
 }
 
 export default function EditUserPage() {
@@ -53,12 +60,17 @@ export default function EditUserPage() {
       return ApiClient.get(`/users/${slug}`)
         .then(res => res.data);
     }, {
-      enabled: !!slug,
-    },
+    enabled: !!slug,
+  },
   );
 
   const onSubmit = (data: FormData) => {
-    const updateData = { hasLeftNode: data.hasLeftNode, type: data.userType };
+    const updateData = { 
+      hasLeftNode: data.hasLeftNode, 
+      type: data.userType,
+      rank: data.rank,
+      name: data.name,
+    };
     ApiClient.patch(`/users/${slug}`, updateData)
       .then(() => queryClient.invalidateQueries())
       .then(() => {
@@ -106,49 +118,64 @@ export default function EditUserPage() {
     <Container p={4} maxW='container.xl' minH='100vh' bg={bgColor}>
       <Navbar />
       <Stack p={5}>
-        <Heading pb={4}>Personal Info</Heading>
-        <Heading size='md'>Basic Info</Heading>
-        <TableContainer>
-          <Table variant='simple'>
-            <Tbody>
-              <Tr>
-                <Td>Rank</Td>
-                <Td>{userInfo.rank}</Td>
-              </Tr>
-              <Tr>
-                <Td>Name</Td>
-                <Td>{userInfo.name}</Td>
-              </Tr>
-              <Tr>
-                <Td>Type</Td>
-                <Td>{userInfo.type}</Td>
-              </Tr>
-              <Tr>
-                <Td>Left Node?</Td>
-                <Td>{userInfo.hasLeftNode ? 'Yes' : 'No'}</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-        <Heading size='md' pt={2}>Edit Info</Heading>
+        <Heading pb={4}>User</Heading>
+        <Heading size='md'>Edit Info</Heading>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl isInvalid={!!errors.userType}>
-            <FormLabel htmlFor='userType'>User Type</FormLabel>
-            <Select id='userType' placeholder='Select option' {...register('userType')}>
-              <option value='Commander'>Commander</option>
-              <option value='S1 Branch'>S1 Branch</option>
-              <option value='S3 Branch'>S3 Branch</option>
-              <option value='S4 Branch'>S4 Branch</option>
-              <option value='Media Team'>Media</option>
-              <option value='Transition'>Transition</option>
-            </Select>
-            <FormErrorMessage>{errors.userType?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor='hasLeftNode'>Left Node?</FormLabel>
-            <Switch id='hasLeftNode' {...register('hasLeftNode')} />
-            <FormErrorMessage>{errors.hasLeftNode?.message}</FormErrorMessage>
-          </FormControl>
+          <TableContainer>
+            <Table variant='simple'>
+              <Tbody>
+                <Tr>
+                  <Td>Rank</Td>
+                  <Td>
+                    <FormControl>
+                      <Editable defaultValue={userInfo.rank}>
+                        <EditablePreview />
+                        <EditableInput id='rank' {...register('rank')} />
+                      </Editable>
+                      <FormErrorMessage>{errors.rank?.message}</FormErrorMessage>
+                    </FormControl>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Name</Td>
+                  <Td>
+                    <FormControl>
+                      <Editable defaultValue={userInfo.name}>
+                        <EditablePreview />
+                        <EditableInput id='name' {...register('name')} />
+                      </Editable>
+                      <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+                    </FormControl>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Type</Td>
+                  <Td>
+                    <FormControl isInvalid={!!errors.userType}>
+                      <Select id='userType' placeholder='Select option' {...register('userType')} defaultValue={userInfo.type}>
+                        <option value='Commander'>Commander</option>
+                        <option value='S1 Branch'>S1 Branch</option>
+                        <option value='S3 Branch'>S3 Branch</option>
+                        <option value='S4 Branch'>S4 Branch</option>
+                        <option value='Media Team'>Media</option>
+                        <option value='Transition'>Transition</option>
+                      </Select>
+                      <FormErrorMessage>{errors.userType?.message}</FormErrorMessage>
+                    </FormControl>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Left Node?</Td>
+                  <Td>
+                    <FormControl>
+                      <Switch id='hasLeftNode' defaultChecked={userInfo.hasLeftNode} {...register('hasLeftNode')} />
+                      <FormErrorMessage>{errors.hasLeftNode?.message}</FormErrorMessage>
+                    </FormControl>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
           <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
             Submit
           </Button>
